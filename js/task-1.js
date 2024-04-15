ff7a66b9d60b57ff8000ad9142c81c82f56765dc
Yuliia <74568169+yutaDV@users.noreply.github.com>
2024-04-15 21:39:45 +0300

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     let totalPrice = quantity * pricePerDroid;
     if (customerCredits >= totalPrice) { 
-        return `You ordered ${quantity}  droids worth ${totalPrice} credits!"`
+        return `You ordered ${quantity}  droids worth ${totalPrice} credits!`
     }
     else { 
         return "Insufficient funds!"
